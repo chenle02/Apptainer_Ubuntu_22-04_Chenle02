cdd1c8a2eb20c8c579c9a0ea84e82cd4a7b5150d
chenle02 <chenle02@gmail.com>
2024-02-07 09:14:59 -0600

    Error in building

---
+++ myzsh.zsh
@@ -0,0 +1,2 @@
+alias build="sudo apptainer build Apptainder_Ubuntu_22-04_Chenle02_nvim_texlive-full_nodejs_gh.sif Apptainder_Ubuntu_22-04_Chenle02.def"
+alias run="sudo apptainer run Apptainder_Ubuntu_22-04_Chenle02.sif"
